93c9310421aa0e440e200c04f8062987f57a89b5
Palmer Cluff <palmercluff@gmail.com>
2020-10-31 22:59:31 -0700

    Adding jQuery and Bootstrap resources

--- environment.sh
+++ environment.sh
@@ -1,5 +1,20 @@
 # For Flask framework
-sudo apt-get install python3-flask
+#sudo apt-get install python3-flask
 
 # For Flask docs
-sudo apt-get install python-flask-doc
+#sudo apt-get install python-flask-doc
+
+# For jQuery
+#sudo apt-get install libjs-jquery
+
+# Copy jQuery files to static directory
+#cp -r /usr/share/javascript/jquery/ personal-website-flask/static/
+
+# For Bootstrap
+#sudo apt-get install libjs-bootstrap
+
+# Copy Bootstrap files to static directory
+#cp -r /usr/share/javascript/bootstrap/ personal-website-flask/static/
+
+# Run the server
+#python index.py
